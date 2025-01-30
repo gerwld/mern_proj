da53e005d2f23291b0bc87a8f85549c6d87842c8
P Jaworski <pjaworski.dev@gmail.com>
2025-01-30 17:01:03 +0100

    Add CORS policy

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'https://www.someadress.com',
+]
+
+module.exports = allowedOrigins;
