bc84c60b22d7d07d4a87d0391df29aae07b6c071
P Jaworski <pjaworski.dev@gmail.com>
2025-02-02 13:29:33 +0100

    added screens, routes, outlets

--- react/src/components/DashLayout.jsx
+++ react/src/components/DashLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import DashHeader from './DashHeader'
+import DashFootert from './DashFootert'
 
 const DashLayout = () => {
     return (
         <>
+            <DashHeader />
             <div className='dash-container'>
                 <Outlet />
+                <DashFootert/>
             </div>
 
         </>
--- react/src/components/Public.jsx
+++ react/src/components/Public.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Public = () => {
     const content = (
         <section className="public">
