dcc68980b597bb5dba609a40aa85d80ac06cdcf8
P Jaworski <pjaworski.dev@gmail.com>
2025-02-03 00:44:17 +0100

    apiSlice upd, etc

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
    <StrictMode>
-      <BrowserRouter>
-         <Routes>
-            <Route path="/*" element={<App />} />
-         </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/*" element={<App />} />
+            </Routes>
+         </BrowserRouter>
+      </Provider>
    </StrictMode>
 );
