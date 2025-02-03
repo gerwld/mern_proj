1605c35f26970b5693e0940d2f4bd4a0012de221
P Jaworski <pjaworski.dev@gmail.com>
2025-02-03 14:40:24 +0100

    upd origins CORS

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:3001',
     'http://localhost:3002',
     'http://localhost:3003',
+    'http://localhost:5173',
     'https://www.someadress.com',
 ]
 
