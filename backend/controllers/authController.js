bfa66e522e414172b93fbade398af8b0e4533597
P Jaworski <pjaworski.dev@gmail.com>
2025-02-04 00:18:32 +0100

    better auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -20,9 +20,35 @@ const login = asyncHandler(async (req, res) => {
     }
 
     const match = await bcrypt.compare(password, foundUser.password);
-    if (!match) return res.status(400).json({ message: "Unauthorized" })
+    if (!match) return res.status(401).json({ message: "Unauthorized" })
 
     res.status(200).json({message: "Authorized"});
+
+    const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": foundUser.username,
+                "roles": foundUser.roles
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expriresIn: '10s'}
+    );
+    const refreshToken = jwt.sign(
+        { "username": foundUser.username }, 
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: '1d'}
+    )
+
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true, // accessible only by web server
+        secure: true, // https
+        sameSite: 'None', // dev, cross-origin
+        maxAge: 7 * 24 * 60 * 60 * 1000 // cookie expiry (7d), better to >1d
+    })
+
+    // Send accessToken containing username and roles
+    res.json({accessToken})
 });
 
 
@@ -30,7 +56,36 @@ const login = asyncHandler(async (req, res) => {
 // @route GET /auth/refresh
 // @acess Public - because access token has expired
 const refresh = async (req, res) => {
+    const cookies = req.cookies;
+    if(!cookies?.jwt) return res.status(401).jsoh({message: "Unauthorized"});
 
+    const refreshToken = cookies.jwt;
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        asyncHandler(async(err, decoded) => {
+            if(err) return res.status(403).json({message: "Forbidden"})
+
+            const foundUser = await User.findOne({username: decoded.username})
+
+            if(!foundUser) return res.status(401).json({message: "Unauthorized"});
+
+            const accessToken = jwt.sign(
+                {
+                    "UserInfo": {
+                        "username": foundUser.username,
+                        "roles": foundUser.roles,
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                {expiresIn: "10s"}
+
+            );
+
+            res.json({accessToken});
+        })
+    )
 }
 
 
@@ -39,7 +94,10 @@ const refresh = async (req, res) => {
 // @route POST /auth/logout
 // @access Public - just to clear cookie if exists
 const logout = async (req, res) => {
-
+    const cookies = req.cookies;
+    if(!cookies.jwt) return res.sendStatus(204) // No content
+    res.clearCookie('jwt', {httpOnly: true, sameSite: 'None', secure: true});
+    res.json({message: "Cookie cleared"})
 }
 
 
